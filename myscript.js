3ef81a1bcf5c92cd855328491b350e92b8a3cb85
JanMrogala <jan.mrogala@seznam.cz>
2021-10-23 12:49:54 +0200

    Clearing up

--- myscript.js
+++ myscript.js
@@ -1,1 +1,4 @@
-
+ document.addEventListener('DOMContentLoaded', function () {
+            var elems = document.querySelectorAll('.carousel');
+            var instances = M.Carousel.init(elems);
+        });
