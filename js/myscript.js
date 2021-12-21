bc226e393cdd4245a32c379d84bd4b51a67dba0b
JanMrogala <jan.mrogala@seznam.cz>
2021-12-21 01:41:57 +0100

    Renamed myScript to index.js

--- js/myscript.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var elems = document.querySelectorAll(".carousel");
-  var instances = M.Carousel.init(elems);
-});
-
-convertImgBtn = document.getElementById("btnConvertImg");
-convertImgBtn.addEventListener("click", convertToRight);
